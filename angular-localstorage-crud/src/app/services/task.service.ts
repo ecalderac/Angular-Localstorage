6470aaff02179aedcbf559ece0fe742ddcccf83d
Eduardo Caldera Coltters <e.calderac@hotmail.com>
2019-11-23 12:34:06 -0300

    Aplicacion finalizada e implementada con localstorage para que la informacion sea persistente, falta agregar el editar

--- angular-localstorage-crud/src/app/services/task.service.ts
+++ angular-localstorage-crud/src/app/services/task.service.ts
@@ -10,20 +10,37 @@ export class TaskService {
   tasks: Task[];
 
   constructor() {
-    /*  this.tasks = [
-        {title: 'write', description: 'I have to write', hide: true},
-        {title: 'write 2', description: 'I have to write 2', hide: true}
-      ];*/
+      this.tasks = [
+        //{title: 'write', description: 'I have to write', hide: true},
+        //{title: 'write 2', description: 'I have to write 2', hide: true}
+      ];
    }
 
     //Obtener tareas
     getTasks(){
-     return this.tasks;
+      if(localStorage.getItem('tasks') === null){
+        return this.tasks;
+      }else{
+        this.tasks = JSON.parse(localStorage.getItem('tasks'));
+        return this.tasks;
+      }
+     
     }
 
     //Agregar tareas
     addTask(task: Task){
       this.tasks.push(task);
+      let tasks: Task[] = [];
+      if(localStorage.getItem('tasks') === null){
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
+
+    
     }
 
     //Eliminar tareas
@@ -32,6 +49,7 @@ export class TaskService {
         {
           if(task == this.tasks[i]){
             this.tasks.splice(i, 1);
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
           }
         }
     }
